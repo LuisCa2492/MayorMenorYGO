339edbb01826ec009cf50b92ee45b4eb8747a947
Luis Miranda Calderón <luismirandac92@gmail.com>
2025-07-28 11:26:01 -0600

    comienzo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import NavBar from '../src/ui/NavBar';
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <NavBar/>
+    </>
+  )
+}
+
+export default App
